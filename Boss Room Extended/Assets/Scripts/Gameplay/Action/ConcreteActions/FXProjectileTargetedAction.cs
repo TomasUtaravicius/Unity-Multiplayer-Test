4e5f4e3bd2a14c702cce22a67ec3a8276f9956e0
TomasUtaravicius <tomasutaravicius@gmail.com>
2023-11-01 19:32:33 +0100

    WIP

--- Boss Room Extended/Assets/Scripts/Gameplay/Action/ConcreteActions/FXProjectileTargetedAction.cs
+++ Boss Room Extended/Assets/Scripts/Gameplay/Action/ConcreteActions/FXProjectileTargetedAction.cs
@@ -26,8 +26,9 @@ namespace Unity.BossRoom.Gameplay.Actions
             m_DamageableTarget = GetDamageableTarget(serverCharacter);
 
             // figure out where the player wants us to aim at...
-            Vector3 targetPos = m_DamageableTarget != null ? m_DamageableTarget.transform.position : m_Data.Position;
+            //Vector3 targetPos = m_DamageableTarget != null ? m_DamageableTarget.transform.position : m_Data.Position;
 
+            Vector3 targetPos = Vector3.zero;
             // then make sure we can actually see that point!
             if (!ActionUtils.HasLineOfSight(serverCharacter.physicsWrapper.Transform.position, targetPos, out Vector3 collidePos))
             {
